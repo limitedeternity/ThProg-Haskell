1f86757a0b23b46ac978029eae9cfa8a0f660533
Marise Hayashi <vyatheslav2707@mail.ru>
2019-10-07 17:57:24 +0300

    Update

--- Lab3/2/3.hs
+++ Lab3/2/3.hs
@@ -1,4 +1,6 @@
-fac n = product [1..n]
+fac n | n <= 0 = 1
+      | otherwise = n * fac(n - 1)
+
 c n m = (fac n) / ((fac (n - m)) * fac m)
 
 test1 = c 5 3 == 10
--- Lab3/2/6(3).hs
+++ Lab3/2/6(3).hs
@@ -1,4 +1,7 @@
-fac n = product [1..n]
-f n = sum [fac k | k <- [0..n]]
+fac n | n <= 0 = 1
+      | otherwise = n * fac (n - 1)
+
+f n | n <= -1 = 0
+    | otherwise = (+) (fac n) (f (n - 1))
 
 test1 = f 3 == fac 0 + fac 1 + fac 2 + fac 3

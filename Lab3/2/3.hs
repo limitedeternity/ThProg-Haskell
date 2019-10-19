f0476d6ca8220895b283e9280cdc2e94d38607c7
Marise Hayashi <vyatheslav2707@mail.ru>
2019-10-19 10:24:50 +0300

    Сделал по красоте

--- Lab3/2/3.hs
+++ Lab3/2/3.hs
@@ -1,7 +1,10 @@
 -- Число способов выбрать n элементов из m-множества без учёта порядка
-c n m | n == 0 = 1
+c n m | n > m = 0
+      | n == 0 = 1
       | n == m = 1
-      | n > m = 0
-      | otherwise = c (n - 1) (m - 1) + c n (m - 1)
+      | otherwise = g ((max n (m - n)) + 1) (min n (m - n))
+      where
+        g l1 l2 | l1 > m && l2 < 1 = 1
+                | otherwise = (l1 / l2) * g (l1 + 1) (l2 - 1)
 
-test1 = c 3 5 == 10
+test1 = c 27 50 == (foldl (*) 1 [28,29..50]) / (foldl (*) 1 [1,2..23])
